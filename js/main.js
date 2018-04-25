5e2d6a90d3e888dede2d9d9bc43df183c69f25b2
SobolevYuri <yura.sobolev.91@gmail.com>
2018-04-25 14:44:12 +0300

    request adaptation (not all)

--- js/main.js
+++ js/main.js
@@ -39,3 +39,26 @@ function showRequest(){
     darkBack.classList.remove('hide-request');
     popupWin.classList.remove('hide-request');
 }
+
+const submitBtn=document.querySelector('button.submit');
+const execBtn=document.querySelector('#execute');
+const darkBgThanks=document.querySelector('div.dark-bg');
+const closetExec=document.querySelector('div.popup-thanks .close-popup');
+const thanksWin=document.querySelector('div.popup-thanks');
+
+submitBtn.addEventListener("click", showThanks);
+darkBgThanks.addEventListener("click", hideThanks);
+closetExec.addEventListener("click", hideThanks);
+execBtn.addEventListener("click", function(){
+    hideRequest();
+    showThanks();
+});
+
+function showThanks(){
+    darkBgThanks.classList.remove('hide-request');
+    thanksWin.classList.remove('hide-request');
+}
+function hideThanks(){
+   darkBgThanks.classList.add('hide-request');
+    thanksWin.classList.add('hide-request');
+    }
