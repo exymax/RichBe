c4931fe9b40ab22eb7f0d69cb29674a2cc6b5b08
SobolevYuri <yura.sobolev.91@gmail.com>
2018-04-25 04:57:23 +0300

    adapted main page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+/*const darkBack=document.querySelector('div.dark-bg');
+const popupWin=document.querySelector('div.request-window');
+const closetReq=document.querySelector('section.request div.close-popup');
+//const closetExec=document.querySelector('div.popup-thanks div.close-popup');
+const openRequest1=document.querySelector("aside button.want-banner-btn");
+const openRequest2=document.querySelector("aside button.select-banner-btn");
+/*const submitBtn=document.querySelector('#submit');
+const execBtn=document.querySelector('#execute');
+
+darkBack.addEventListener("click", hideRequest);
+closetReq.addEventListener("click", hideRequest);
+openRequest1.addEventListener("click", showRequest);
+openRequest2.addEventListener("click", showRequest);
+
+function hideRequest(){
+darkBack.classList.add('hide-request');
+popupWin.classList.add('hide-request');
+}
+function showRequest(){
+    darkBack.classList.remove('hide-request');
+    popupWin.classList.remove('hide-request');
+}*/
 const darkBack=document.querySelector('div.dark-bg');
 const popupWin=document.querySelector('div.request-window');
 const closet=document.querySelector('div.close-popup');
