dfc59450ca1aad4334c209eed2f9b9db00243513
SobolevYuri <yura.sobolev.91@gmail.com>
2018-04-25 15:36:11 +0300

    client Demo

--- js/main.js
+++ js/main.js
@@ -1,30 +1,9 @@
-/*const darkBack=document.querySelector('div.dark-bg');
-const popupWin=document.querySelector('div.request-window');
-const closetReq=document.querySelector('section.request div.close-popup');
-//const closetExec=document.querySelector('div.popup-thanks div.close-popup');
-const openRequest1=document.querySelector("aside button.want-banner-btn");
-const openRequest2=document.querySelector("aside button.select-banner-btn");
-/*const submitBtn=document.querySelector('#submit');
-const execBtn=document.querySelector('#execute');
 
-darkBack.addEventListener("click", hideRequest);
-closetReq.addEventListener("click", hideRequest);
-openRequest1.addEventListener("click", showRequest);
-openRequest2.addEventListener("click", showRequest);
-
-function hideRequest(){
-darkBack.classList.add('hide-request');
-popupWin.classList.add('hide-request');
-}
-function showRequest(){
-    darkBack.classList.remove('hide-request');
-    popupWin.classList.remove('hide-request');
-}*/
-const darkBack=document.querySelector('div.dark-bg');
-const popupWin=document.querySelector('div.request-window');
-const closet=document.querySelector('div.close-popup');
-const openRequest1=document.querySelector("aside button.want-banner-btn");
-const openRequest2=document.querySelector("aside button.select-banner-btn");
+var darkBack=document.querySelector('div.dark-bg');
+var popupWin=document.querySelector('div.request-window');
+var closet=document.querySelector('div.close-popup');
+var openRequest1=document.querySelector("aside button.want-banner-btn");
+var openRequest2=document.querySelector("aside button.select-banner-btn");
 
 darkBack.addEventListener("click", hideRequest);
 closet.addEventListener("click", hideRequest);
@@ -40,11 +19,11 @@ function showRequest(){
     popupWin.classList.remove('hide-request');
 }
 
-const submitBtn=document.querySelector('button.submit');
-const execBtn=document.querySelector('#execute');
-const darkBgThanks=document.querySelector('div.dark-bg');
-const closetExec=document.querySelector('div.popup-thanks .close-popup');
-const thanksWin=document.querySelector('div.popup-thanks');
+var submitBtn=document.querySelector('#submit');
+var execBtn=document.querySelector('#execute');
+var darkBgThanks=document.querySelector('div.dark-bg-popup');
+var closetExec=document.querySelector('div.popup-thanks .close-popup');
+var thanksWin=document.querySelector('div.popup-thanks');
 
 submitBtn.addEventListener("click", showThanks);
 darkBgThanks.addEventListener("click", hideThanks);
@@ -54,11 +33,13 @@ execBtn.addEventListener("click", function(){
     showThanks();
 });
 
-function showThanks(){
+function showThanks(e){
+   e.preventDefault();
     darkBgThanks.classList.remove('hide-request');
     thanksWin.classList.remove('hide-request');
 }
+
 function hideThanks(){
    darkBgThanks.classList.add('hide-request');
     thanksWin.classList.add('hide-request');
-    }
+}
