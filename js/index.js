2d19c88c1db2e29c73b6a6ecf72dab9d88c9a9b9
SobolevYuri <yura.sobolev.91@gmail.com>
2018-04-27 02:21:02 +0300

    Almost done validation

--- js/index.js
+++ js/index.js
@@ -1,58 +1,28 @@
-$('.phone').mask('+7 (999) 999-99-99');
 
-const kirylicPattern = /[А-Яа-яЁё\s]+/;
-var submitBtn=document.querySelector('#submit');
-var execBtn=document.querySelector('#execute');
-var fioMain=document.querySelector('#fio-main');
-var phoneMain=document.querySelector('#phone-main');
-var fioReq=document.querySelector('#fio-req');
-var phoneReq=document.querySelector('#phone-req');
-
-function validate() {
-  const target = this.event.target;
-  const eventValue = this.event.data;
-
-switch (target.name) {
-    case 'fio':
-      if (!eventValue.match(kirylicPattern)) {
-        target.value = target.value.slice(0, target.value.length-1);
-      }
-      break;
-    case 'mail':
-      console.log(this.event.data);
-      if (eventValue.match(kirylicPattern)) {
-        target.value = target.value.slice(0, target.value.length-1);
-      }
-      break;
-    default: return;
-  }
-}
-/*function changes(){
-
-}*/
-
-const emailSub = document.querySelector('section.request .email');
-const emailExec = document.querySelector('div.request-window .email');
+emailSub.onfocus = function (event) {
+    event.target.value="";
+};
+emailExec.onfocus = function (event) {
+    event.target.value="";
+};
 
 submitBtn.addEventListener('click', submitHandlerSub);
 execBtn.addEventListener('click', submitHandlerExec);
-emailSub.onfocus = function (event) {
-  event.target.value="";
-};
-emailExec.onfocus = function (event) {
-  event.target.value="";
-};
 
-function submitHandlerSub(event) {
-  console.log(emailSub.value);
-  if (!emailSub.value.match(/^[a-zA-Z0-9][\w/.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w/.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z/.]*[a-zA-Z]$/)) {
-    emailSub.value = "Некорретный E-mail!";
-    event.preventDefault();
-  }
-}
 function submitHandlerExec(event) {
   if (!emailExec.value.match(/^[a-zA-Z0-9][\w/.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w/.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z/.]*[a-zA-Z]$/)) {
     emailExec.value = "Некорретный E-mail!";
     event.preventDefault();
   }
 }
+
+const emailField = document.querySelector('.email');
+emailField.onfocus = function (event) {
+  event.target.value="";
+};
+
+
+var fioMain=document.querySelector('#fio-main');
+var phoneMain=document.querySelector('#phone-main');
+var fioReq=document.querySelector('#fio-req');
+var phoneReq=document.querySelector('#phone-req');
--- js/main.js
+++ js/main.js
@@ -7,12 +7,22 @@ var openRequest2=document.querySelector("aside button.select-banner-btn");
 var darkBgConf=document.querySelector('div.dark-bg-confid');
 var closetConf=document.querySelector('div.confid-popup .close-popup');
 var openConf=document.querySelector('.confirm-politic span');
+var openConfRequest=document.querySelector('.request-window .confirm-politic span');
 var Confid=document.querySelector('.confid-popup');
+
+var submitBtn=document.querySelector('#submit');
+var execBtn=document.querySelector('#execute');
+var darkBgThanks=document.querySelector('div.dark-bg-popup');
+var closetExec=document.querySelector('div.popup-thanks .close-popup');
+var thanksWin=document.querySelector('div.popup-thanks');
+
+const emailSub = document.querySelector('section.request .email');
+const emailExec = document.querySelector('div.request-window .email');
 
 darkBgConf.addEventListener("click", hideConfid);
 closetConf.addEventListener("click", hideConfid);
 openConf.addEventListener("click", showConfid);
-
+openConfRequest.addEventListener("click", showConfid);
 function hideConfid(){
     darkBgConf.classList.add('hide-request');
     Confid.classList.add('hide-request');
@@ -30,24 +40,22 @@ openRequest2.addEventListener("click", showRequest);
 function hideRequest(){
 darkBack.classList.add('hide-request');
 popupWin.classList.add('hide-request');
+emailSub.value = "";
+emailExec.value = "";
 }
 function showRequest(){
     darkBack.classList.remove('hide-request');
     popupWin.classList.remove('hide-request');
 }
 
-var submitBtn=document.querySelector('#submit');
-var execBtn=document.querySelector('#execute');
-var darkBgThanks=document.querySelector('div.dark-bg-popup');
-var closetExec=document.querySelector('div.popup-thanks .close-popup');
-var thanksWin=document.querySelector('div.popup-thanks');
-
-submitBtn.addEventListener("click", showThanksSub);
+submitBtn.addEventListener("click", () => {
+    //showThanksSub();
+    submitHandler(emailSub);
+});
 darkBgThanks.addEventListener("click", hideThanks);
 closetExec.addEventListener("click", hideThanks);
 execBtn.addEventListener("click", function(){
-    hideRequest();
-    showThanksExec();
+    submitHandler(emailExec);
 });
 function showThanksExec(){
     darkBgThanks.classList.remove('hide-request');
@@ -62,4 +70,48 @@ function showThanksSub(e){
 function hideThanks(){
    darkBgThanks.classList.add('hide-request');
     thanksWin.classList.add('hide-request');
+}
+
+
+
+
+
+$('.phone').mask('+7 (999) 999-99-99');
+
+const kirylicPattern = /[А-Яа-яЁё\s]+/;
+
+function validate() {
+  const target = this.event.target;
+  const eventValue = this.event.data;
+
+switch (target.name) {
+    case 'fio':
+      if (!eventValue.match(kirylicPattern)) {
+        target.value = target.value.slice(0, target.value.length-1);
+      }
+      break;
+    case 'mail':
+      console.log(this.event.data);
+      if (eventValue.match(kirylicPattern)) {
+        target.value = target.value.slice(0, target.value.length-1);
+      }
+      break;
+    default: return;
+  }
+  
+}
+
+function cleaner() {
+    console.log(this.event.target);
+    this.event.target.value="";
+}
+
+function submitHandler(emailField) {
+  if (!emailField.value.match(/^[a-zA-Z0-9][\w/.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w/.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z/.]*[a-zA-Z]$/)) {
+    emailField.value = "Incorrect email!";
+    this.event.preventDefault();
+  } else {
+    hideRequest();
+    showThanksExec();
+  }
 }
