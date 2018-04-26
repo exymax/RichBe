7119f80d57e7f0bc29f9ffcaf75ce6883c6ce3b7
SobolevYuri <yura.sobolev.91@gmail.com>
2018-04-26 15:31:39 +0300

    valid don't work

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 $('.phone').mask('+7 (999) 999-99-99');
 
 const kirylicPattern = /[А-Яа-яЁё\s]+/;
+var submitBtn=document.querySelector('#submit');
+var execBtn=document.querySelector('#execute');
+var fioMain=document.querySelector('#fio-main');
+var phoneMain=document.querySelector('#phone-main');
+var fioReq=document.querySelector('#fio-req');
+var phoneReq=document.querySelector('#phone-req');
 
 function validate() {
   const target = this.event.target;
   const eventValue = this.event.data;
-  switch (target.name) {
+
+switch (target.name) {
     case 'fio':
       if (!eventValue.match(kirylicPattern)) {
         target.value = target.value.slice(0, target.value.length-1);
@@ -20,15 +27,32 @@ function validate() {
     default: return;
   }
 }
+/*function changes(){
 
-const emailField = document.querySelector('.email');
-emailField.onfocus = function (event) {
+}*/
+
+const emailSub = document.querySelector('section.request .email');
+const emailExec = document.querySelector('div.request-window .email');
+
+submitBtn.addEventListener('click', submitHandlerSub);
+execBtn.addEventListener('click', submitHandlerExec);
+emailSub.onfocus = function (event) {
+  event.target.value="";
+};
+emailExec.onfocus = function (event) {
   event.target.value="";
 };
 
-function submitHandler() {
-  if (!emailField.value.match(/^[a-zA-Z0-9][\w/.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w/.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z/.]*[a-zA-Z]$/)) {
-    emailField.value = "Некорретный E-mail!";
-    this.event.preventDefault();
+function submitHandlerSub(event) {
+  console.log(emailSub.value);
+  if (!emailSub.value.match(/^[a-zA-Z0-9][\w/.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w/.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z/.]*[a-zA-Z]$/)) {
+    emailSub.value = "Некорретный E-mail!";
+    event.preventDefault();
+  }
+}
+function submitHandlerExec(event) {
+  if (!emailExec.value.match(/^[a-zA-Z0-9][\w/.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w/.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z/.]*[a-zA-Z]$/)) {
+    emailExec.value = "Некорретный E-mail!";
+    event.preventDefault();
   }
 }
